bb40469901a81caab47647c0104b683705d7b6b6
GurmanpreetKaur23 <119606083+GurmanpreetKaur23@users.noreply.github.com>
2024-06-09 17:23:52 +0530

    Create output.java

---
+++ DAY 1/output.java
@@ -0,0 +1,12 @@
+public class output {
+    public static void main(String[] args) {
+        System.out.print("hello");
+
+        // to print in next line
+        System.out.println() ;
+        System.out.println("hello world");
+        System.out.print("OK\n");
+    }
+}
+
+
