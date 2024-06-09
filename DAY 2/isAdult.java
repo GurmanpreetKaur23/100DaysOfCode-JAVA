e6fc36e989bdd95aad025e04f3cdfd5b0a2447e9
GurmanpreetKaur23 <119606083+GurmanpreetKaur23@users.noreply.github.com>
2024-06-09 17:44:52 +0530

    Create isAdult.java

---
+++ DAY 2/isAdult.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+public class isAdult {
+    public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in) ;
+
+    System.out.println("Enter your age : ") ;
+    int age = sc.nextInt() ;
+    if(age>18) {
+        System.out.println("ADULT");
+    }
+    else {
+        System.out.println("NOT ADULT");
+    }
+    }
+    
+}
