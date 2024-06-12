50204e1c6ed631bd55acfa6b29f3da6aa74e7fc7
GurmanpreetKaur23 <119606083+GurmanpreetKaur23@users.noreply.github.com>
2024-06-12 23:14:35 +0530

    Create 34.java

---
+++ DAY 5/LEETCODE/34.java
@@ -0,0 +1,51 @@
+class Solution {
+    private int firstOcc(int[] nums , int target , int start , int end) {
+        start = 0 ;
+        end = nums.length-1 ;
+        int pos = -1 ;
+        while(start<=end) {
+            int mid = (start+end)/2 ;
+            if(nums[mid]==target) {
+                pos = mid ;
+                end = mid - 1 ;
+            }
+            else if(nums[mid]>target) {
+                end = mid - 1;
+            }
+            else if(nums[mid]<target) {
+                start = mid + 1 ;
+            }
+        }
+        return pos ;
+    }
+
+    private int LastOcc(int[] nums , int target , int start , int end) {
+        start = 0 ;
+        end = nums.length-1 ;
+        int pos = -1 ;
+        while(start<=end) {
+            int mid = (start+end)/2 ;
+            if(nums[mid]==target) {
+                pos = mid ;
+                start = mid + 1 ;
+            }
+            else if(nums[mid]>target) {
+                end = mid - 1;
+            }
+            else if(nums[mid]<target) {
+                start = mid + 1 ;
+            }
+        }
+        return pos ;
+    }
+        public int[] searchRange(int[] nums, int target) {
+        int res[] = {-1 , -1} ;
+        int first = firstOcc(nums , target,0,nums.length-1) ;
+        int last = LastOcc(nums , target , 0 , nums.length-1) ;
+
+        res[0] = first ; 
+        res[1] = last ;
+        return res ;
+    }
+    
+}
