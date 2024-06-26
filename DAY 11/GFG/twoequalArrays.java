9fc63fbfcf85c65dbd1582c04a6984661201cb40
GurmanpreetKaur23 <119606083+GurmanpreetKaur23@users.noreply.github.com>
2024-06-26 22:28:48 +0530

    Create twoequalArrays.java

---
+++ DAY 11/GFG/twoequalArrays.java
@@ -0,0 +1,62 @@
+// https://www.geeksforgeeks.org/problems/check-if-two-arrays-are-equal-or-not3847/1?page=1&difficulty%255B%255D=-1&category%255B%255D=Arrays&sortBy=submissions
+
+
+//{ Driver Code Starts
+//Initial Template for Java
+
+
+/*package whatever //do not write package name here */
+
+import java.io.*;
+
+import java.util.*;
+class GFG {
+	public static void main (String[] args) {
+		Scanner sc=new Scanner(System.in);
+		
+		long t=sc.nextLong();
+		
+		while(t-->0)
+		{
+		  int n=sc.nextInt();
+		  long arr[]=new long[n];
+		  long brr[]=new long[n];
+		  
+		  for(int i=0;i<n;i++)
+		  {
+		      arr[i]=sc.nextLong();
+		  }
+		  
+		  for(int i=0;i<n;i++)
+		  {
+		      brr[i]=sc.nextLong();
+		  }
+		  Solution ob = new Solution();
+		  System.out.println(ob.check(arr,brr,n)==true?"1":"0");
+		  
+		  
+		}
+	}
+}
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+class Solution{
+    //Function to check if two arrays are equal or not.
+    public static boolean check(long A[],long B[],int N)
+    {
+        //Your code here
+        Arrays.sort(A) ;
+        Arrays.sort(B) ;
+        // for(int i=0 ; i<N ; i++) {
+        //     for(int j =0 ; j<N ; j++) {
+        //         if(A[i]==B[j]) return true ;
+        //     }
+        // }
+        // return false ;
+        
+        return Arrays.equals(A,B) ;
+    }
+}
