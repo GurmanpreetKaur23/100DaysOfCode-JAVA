6da649b9e29d283852c46bc790d999935d29da64
GurmanpreetKaur23 <119606083+GurmanpreetKaur23@users.noreply.github.com>
2024-07-02 22:34:19 +0530

    Create Cricket World Cup Qualifier.java

---
+++ DAY 16/CodeChef/Cricket World Cup Qualifier.java
@@ -0,0 +1,18 @@
+// https://www.codechef.com/practice/course/basic-programming-concepts/DIFF500/problems/CWC23QUALIF
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		Scanner sc = new Scanner(System.in) ;
+		int X = sc.nextInt() ;
+		if(X>=12) System.out.print("Yes") ;
+		else System.out.print("No") ;
+
+	}
+}
