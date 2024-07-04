80432f331b4790414f65183dd271f43f10343e07
GurmanpreetKaur23 <119606083+GurmanpreetKaur23@users.noreply.github.com>
2024-07-04 19:54:13 +0530

    Create printf.java

---
+++ DAY 17/printf.java
@@ -0,0 +1,9 @@
+public class printf {
+   public static void main(String[] args) {
+    int i = 5 ;
+    int j = 7 ;
+    int k = i+j ;
+
+    System.out.printf("addition of %d & %d is %d",i,j,k);
+   }  
+}
