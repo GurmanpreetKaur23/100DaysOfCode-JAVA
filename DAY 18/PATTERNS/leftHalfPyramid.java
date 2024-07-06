60d7dc8b7555be734fd27da60408c14dc72f6ce3
GurmanpreetKaur23 <119606083+GurmanpreetKaur23@users.noreply.github.com>
2024-07-07 00:52:46 +0530

    Create leftHalfPyramid.java

---
+++ DAY 18/PATTERNS/leftHalfPyramid.java
@@ -0,0 +1,24 @@
+package Patterns;
+import java.util.* ;
+
+//       *
+//     * *
+//   * * *
+// * * * *
+
+public class leftHalfPyramid {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in) ;
+        int n = sc.nextInt() ;
+
+        for(int i=n ; i>=1 ; i--) {
+            for(int j = 1 ; j<i;j++) {
+                System.out.print(" ");
+            }
+            for(int j = 0 ; j<=n-i ; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }    
+}
