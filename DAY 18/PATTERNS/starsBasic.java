613ca9991048f839ac77491f94a5b577d24556ed
GurmanpreetKaur23 <119606083+GurmanpreetKaur23@users.noreply.github.com>
2024-07-06 23:56:42 +0530

    Create starsBasic.java

---
+++ DAY 18/PATTERNS/starsBasic.java
@@ -0,0 +1,22 @@
+package Patterns;
+import java.util.* ;
+
+// * * * *
+// * * * * 
+// * * * * 
+// * * * * 
+
+public class starsBasic {
+    public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in) ;
+    int n = sc.nextInt() ; // rows
+    int m = sc.nextInt() ; // columns
+
+    for(int i=0 ; i<n ; i++) {
+        for(int j=0 ; j<m ; j++) {
+            System.out.print(" * ");
+        }
+        System.out.println();
+    }
+    }
+}
